c40a5fc897b9f7d64ea73046dfb2418cb5ff65c4
Davros <labfdquantum@gmail.com>
2024-05-15 16:19:18 -0300

    added a new measurement methods

--- src/measurements/MeasurementSetup.py
+++ src/measurements/MeasurementSetup.py
@@ -10,7 +10,13 @@ from instruments.SIM928_driver import * # Voltage source
 from instruments.SIM970_driver import * # Voltage meter
 from importlib import reload # comando para reimportar mais facilmente algum arquivo
 
-from measurements.Pulse import Pulse,Envelope
+
+from measurements.waveforms.SquarePulse import *
+from measurements.waveforms.SquareGaussianPulse import *
+from measurements.waveforms.DragPulse import *
+from measurements.waveforms.ZeroPulse import *
+from measurements.waveforms.GaussianPulse import *
+
 from measurements.PulseSequence import PulseSequence
 from measurements.DataChannelManager import DataChannelManager
 
@@ -165,6 +171,23 @@ class MeasurementSetup:
 
         self.ms = DataChannelManager('AWG')
 
+        self.ms.clearAwgChannel()
+        self.ms.labelAwgChannel(channel = 1, # o canal do awg
+                        channelName = 'm',  # o seu nome em relação ao sequenciador
+                        freq = 0, # a frequência real a ser utilizada por esse canal
+                        markerValue = 2, # Esse valor indica qual marker vai ser ligado enquanto estiver ocorendo algum pulso nesse canal
+                        markers = True) # Afirma que esse canal é usado para configurar markers
+
+        self.ms.labelAwgChannel(channel = 2, # o canal do awg
+                        channelName = 'Q',  # o seu nome em relação ao sequenciador
+                        freq = 0,
+                        markerValue = 1)
+        
+        self.ms.labelAwgChannel(channel = 1, # o canal do awg
+                        channelName = 'I',  # o seu nome em relação ao sequenciador
+                        freq = 0,
+                        markerValue = 1, # Esse valor indica qual marker vai ser ligado enquanto estiver ocorendo algum pulso nesse canal
+                        markers = True)
 
     def capture(self):
         I,Q = self.inst_alazar.capture(preTriggerSamples = self.alazar_params['preTriggerSamples'],                                       
@@ -184,68 +207,59 @@ class MeasurementSetup:
         
         return I,Q
 
-
-    def prepareForCavityMeasure(self, MeasurementPulseLength, AttenuationValue):
+    def preparePulseSequence(self, s):
         self.inst_awg.stop()
-        self.inst_RFsourceMeasurement.stop_rf()
-
-        self.RFMeasurementLength = MeasurementPulseLength
-        self.attenuation = AttenuationValue
+        
+        self.sequence = s
 
         samplingRate = self.DefaultAlazarSamplingRate/self.alazar_params['decimationValue']
 
-        # O número pontos por aquisição depende do sampling rate. Tem que ser múltiplos de 256.
+        self.RFMeasurementLength = self.sequence.channels['m']['pulses'][0].length
         self.alazar_params['postTriggerSamples'] = int(self.RFMeasurementLength*samplingRate/256)*256    # quantos pontos depois do trigger.
 
-        mp = Pulse(length = self.RFMeasurementLength, # duração
-           amplitude = 1, # amplitude. não utilizado para nada
-           frequency = 70e6, # frequência, o de medida não utilizado para nada
-           phase = 0, # a fase
-           envelope='zero') # o seu formanto, gaussiano, quadrado ou quadrado gaussiano
-        
-        self.sequence = PulseSequence('Cavity') # dá um nome da medida
-        self.sequence.startup_delay = 1e-6 # um delay para ligar antecipadamente a fonte de excitação. liga a fonte antecipadamente por esse valor antes do primeiro pulso de excitação
-
-        self.sequence.clear()
-
-        self.sequence.add(mp,'m') # Adiciona o pulso ao sequenciador e o conecta a um canal, no caso o canal "m"
-        # Esse canal vai ser especificado a um dos canais do awg. Isso ainda não aconteceu
-
-        
-
-        #Aqui conectamos canais usados pelo sequenciado aos canais do AWG
-        # o mesmo canal do AWG pode ser usado por muitos canais do sequenciador
-        self.ms.clearAwgChannel()
-        self.ms.labelAwgChannel(channel = 1, # o canal do awg
-                                channelName = 'm',  # o seu nome em relação ao sequenciador
-                                freq = 0, # a frequência real a ser utilizada por esse canal. Se 0, significa que o sinal vai ser apenas 0
-                                markerValue = 2, # Esse valor indica qual marker vai ser ligado enquanto estiver ocorendo algum pulso nesse canal
-                                markers = True) # Afirma que esse canal é usado para configurar markers
-
-
-        # essa função gera os dados aceitos pelo awg para uma medida total especificada
-        # o tempo total deve ser maior que o tempo total da sequência dos pulsis
-        channelData = self.ms.prepareChannelData(self.inst_awg, self.sequence, self.TotalMeasurementLength) # add total length, pulses and relaxation to alloc the necessary bytes in memory
-
+        self.channelData = self.ms.prepareChannelData(self.inst_awg, self.sequence, self.TotalMeasurementLength) # add total length, pulses and relaxation to alloc the necessary bytes in memory
 
         # deleta toda memória do awg
         self.inst_awg.clearMemory()
         sleep(0.05)
 
         # Aloca o espaço necessário para a medida
-        self.ms.allocAwgMemory(self.inst_awg,channelData)
+        self.ms.allocAwgMemory(self.inst_awg,self.channelData)
         sleep(0.05)
 
 
         # Carrega os pulsos ao awg
-        self.ms.loadChannelDataToAwg(self.inst_awg,channelData,'m')
+        self.ms.loadChannelDataToAwg(self.inst_awg,self.channelData,'m')
+        sleep(0.05)
+
+        # Carrega os pulsos ao awg
+        self.ms.loadChannelDataToAwg(self.inst_awg,self.channelData,'Q')
+        sleep(0.05)
+
+        # Carrega os pulsos ao awg
+        self.ms.loadChannelDataToAwg(self.inst_awg,self.channelData,'I')
         sleep(0.05)
 
         # adiciona o tempo de antecipação da fonte de excitação
-        self.ms.setInstrumentsMarker(self.inst_awg,channelData)
+        self.ms.setInstrumentsMarker(self.inst_awg,self.channelData)
 
+    def prepareForCavityMeasure(self, AttenuationValue):
+        self.inst_awg.stop()
+        self.inst_RFsourceMeasurement.stop_rf()
 
+        self.attenuation = AttenuationValue
 
+        mp = ZeroPulse(length = self.RFMeasurementLength)
+        
+        s1 = PulseSequence('Cavity') # dá um nome da medida
+        s1.startup_delay = 1e-6 # um delay para ligar antecipadamente a fonte de excitação. liga a fonte antecipadamente por esse valor antes do primeiro pulso de excitação
+
+        s1.clear()
+
+        s1.add(mp,'m') # Adiciona o pulso ao sequenciador e o conecta a um canal, no caso o canal "m"
+        # Esse canal vai ser especificado a um dos canais do awg. Isso ainda não aconteceu
+
+        self.preparePulseSequence(s1)
             
     def measureCavity(self, freqs):
         self.inst_awg.start()
@@ -323,7 +337,6 @@ class MeasurementSetup:
 
         return Is,Qs,mags
 
-
     def prepareForTwotoneMeasure(self, ExcitationPulseLength):
         self.inst_awg.stop()
         self.inst_RFsourceMeasurement.stop_rf()
@@ -331,67 +344,23 @@ class MeasurementSetup:
 
         self.ExcitationPulseLength = ExcitationPulseLength
 
-
         self.inst_RFsourceMeasurement.set_frequency(self.MeasurementFrequency-self.Measurement_IF)
 
-        p1 = Envelope(length = self.ExcitationPulseLength,
-              amplitude = 1,
-              envelope='square') # define o formato gaussiano da medida
+        p1 = SquarePulse(length = self.ExcitationPulseLength)
 
-
-
-        mp = Pulse(length = self.RFMeasurementLength, # duração
-                amplitude = 1, # amplitude. não utilizado para nada
-                frequency = 70e6, # frequência, o de medida não utilizado para nada
-                phase = 0, # a fase
-                envelope='zero') # o seu formanto, gaussiano, quadrado ou quadrado gaussiano
+        mp = ZeroPulse(length = self.RFMeasurementLength)
         
         # create pulse sequence
-        self.sequence = PulseSequence('Twotone') # dá um nome da medida
-        self.sequence.startup_delay = 1e-6 # um delay para ligar antecipadamente a fonte de excitação. liga a fonte antecipadamente por esse valor antes do primeiro pulso de excitação
+        s1 = PulseSequence('Twotone') # dá um nome da medida
+        s1.startup_delay = 1e-6 # um delay para ligar antecipadamente a fonte de excitação. liga a fonte antecipadamente por esse valor antes do primeiro pulso de excitação
 
-        self.sequence.clear()
+        s1.clear()
 
-        self.sequence.add(p1,'Q',self.RFMeasurementLength) 
-        self.sequence.add(mp,'m') # Adiciona o pulso ao sequenciador e o conecta a um canal, no caso o canal "m"
+        s1.add(p1,'Q',self.RFMeasurementLength) 
+        s1.add(mp,'m') # Adiciona o pulso ao sequenciador e o conecta a um canal, no caso o canal "m"
         # Esse canal vai ser especificado a um dos canais do awg. Isso ainda não aconteceu
 
-
-        self.ms.clearAwgChannel()
-        self.ms.labelAwgChannel(channel = 1, # o canal do awg
-                        channelName = 'm',  # o seu nome em relação ao sequenciador
-                        freq = 0, # a frequência real a ser utilizada por esse canal
-                        markerValue = 2, # Esse valor indica qual marker vai ser ligado enquanto estiver ocorendo algum pulso nesse canal
-                        markers = True) # Afirma que esse canal é usado para configurar markers
-
-        self.ms.labelAwgChannel(channel = 2, # o canal do awg
-                        channelName = 'Q',  # o seu nome em relação ao sequenciador
-                        freq = 0,
-                        markerValue = 1)
-
-
-        # essa função gera os dados aceitos pelo awg para uma medida total especificada
-        # o tempo total deve ser maior que o tempo total da sequência dos pulsis
-        channelData = self.ms.prepareChannelData(self.inst_awg, self.sequence, self.TotalMeasurementLength) # add total length, pulses and relaxation to alloc the necessary bytes in memory
-
-
-        # deleta toda memória do awg
-        self.inst_awg.clearMemory()
-        sleep(0.05)
-
-        # Aloca o espaço necessário para a medida
-        self.ms.allocAwgMemory(self.inst_awg,channelData)
-        sleep(0.05)
-
-        # Carrega os pulsos ao awg
-        self.ms.loadChannelDataToAwg(self.inst_awg,channelData,'m')
-        sleep(0.05)
-
-        self.ms.loadChannelDataToAwg(self.inst_awg,channelData,'Q')
-        sleep(0.05)
-
-        # adiciona o tempo de antecipação da fonte de excitação
-        self.ms.setInstrumentsMarker(self.inst_awg,channelData)
+        self.preparePulseSequence(s1)
 
     def measureTwotone(self, qfreqs):
         self.inst_awg.start()
@@ -498,69 +467,22 @@ class MeasurementSetup:
         self.inst_RFsourceExcitation.set_frequency(self.ExcitationFrequency)
         self.inst_RFsourceMeasurement.set_frequency(self.MeasurementFrequency-self.Measurement_IF)
 
-        p1 = Envelope(length = self.HalfPiPulse,
-              amplitude = 1,
-              envelope='square') # define o formato gaussiano da medida
-        
-        p2 = Envelope(length = self.HalfPiPulse,
-              amplitude = 1,
-              envelope='square') # define o formato gaussiano da medida
-
-
-
-        mp = Pulse(length = self.RFMeasurementLength, # duração
-                amplitude = 1, # amplitude. não utilizado para nada
-                frequency = 70e6, # frequência, o de medida não utilizado para nada
-                phase = 0, # a fase
-                envelope='zero') # o seu formanto, gaussiano, quadrado ou quadrado gaussiano
+        p1 = SquarePulse(length = self.HalfPiPulse)
+        p2 = SquarePulse(length = self.HalfPiPulse)
+        mp = ZeroPulse(length = self.RFMeasurementLength)
         
         # create pulse sequence
-        self.sequence = PulseSequence('Twotone') # dá um nome da medida
-        self.sequence.startup_delay = 1e-6 # um delay para ligar antecipadamente a fonte de excitação. liga a fonte antecipadamente por esse valor antes do primeiro pulso de excitação
+        s1 = PulseSequence('Ramsey') # dá um nome da medida
+        s1.startup_delay = 1e-6 # um delay para ligar antecipadamente a fonte de excitação. liga a fonte antecipadamente por esse valor antes do primeiro pulso de excitação
 
-        self.sequence.clear()
+        s1.clear()
 
-        self.sequence.add(p1,'Q') 
-        self.sequence.add(p2,'Q',self.RFMeasurementLength) 
-        self.sequence.add(mp,'m') # Adiciona o pulso ao sequenciador e o conecta a um canal, no caso o canal "m"
+        s1.add(p1,'Q') 
+        s1.add(p2,'Q',self.RFMeasurementLength) 
+        s1.add(mp,'m') # Adiciona o pulso ao sequenciador e o conecta a um canal, no caso o canal "m"
         # Esse canal vai ser especificado a um dos canais do awg. Isso ainda não aconteceu
 
-
-        self.ms.clearAwgChannel()
-        self.ms.labelAwgChannel(channel = 1, # o canal do awg
-                        channelName = 'm',  # o seu nome em relação ao sequenciador
-                        freq = 0, # a frequência real a ser utilizada por esse canal
-                        markerValue = 2, # Esse valor indica qual marker vai ser ligado enquanto estiver ocorendo algum pulso nesse canal
-                        markers = True) # Afirma que esse canal é usado para configurar markers
-
-        self.ms.labelAwgChannel(channel = 2, # o canal do awg
-                        channelName = 'Q',  # o seu nome em relação ao sequenciador
-                        freq = 0,
-                        markerValue = 1)
-
-
-        # essa função gera os dados aceitos pelo awg para uma medida total especificada
-        # o tempo total deve ser maior que o tempo total da sequência dos pulsis
-        self.channelData = self.ms.prepareChannelData(self.inst_awg, self.sequence, self.TotalMeasurementLength) # add total length, pulses and relaxation to alloc the necessary bytes in memory
-
-
-        # deleta toda memória do awg
-        self.inst_awg.clearMemory()
-        sleep(0.05)
-
-        # Aloca o espaço necessário para a medida
-        self.ms.allocAwgMemory(self.inst_awg,self.channelData)
-        sleep(0.05)
-
-        # Carrega os pulsos ao awg
-        self.ms.loadChannelDataToAwg(self.inst_awg,self.channelData,'m')
-        sleep(0.05)
-
-        self.ms.loadChannelDataToAwg(self.inst_awg,self.channelData,'Q')
-        sleep(0.05)
-
-        # adiciona o tempo de antecipação da fonte de excitação
-        self.ms.setInstrumentsMarker(self.inst_awg,self.channelData)
+        self.preparePulseSequence(s1)
 
     def measureRamsey(self, delays):
         self.inst_awg.start()
@@ -662,3 +584,159 @@ class MeasurementSetup:
         self.inst_awg.stop()
         self.inst_RFsourceExcitation.stop_rf()
         self.inst_RFsourceMeasurement.stop_rf()
+
+        return Is,Qs,mags
+
+    def prepareRelaxationMeasure(self, PiPulse):
+        self.inst_awg.stop()
+        self.inst_RFsourceMeasurement.stop_rf()
+        self.inst_RFsourceExcitation.stop_rf()
+
+        self.inst_voltsource.turn_on()
+        self.inst_voltsource.ramp_voltage(self.fluxValue)
+
+
+        self.PiPulse = PiPulse
+
+        self.inst_RFsourceExcitation.set_frequency(self.ExcitationFrequency)
+        self.inst_RFsourceMeasurement.set_frequency(self.MeasurementFrequency-self.Measurement_IF)
+
+        p1 = SquarePulse(length = self.PiPulse) 
+        mp = ZeroPulse(length = self.RFMeasurementLength) 
+        
+        # create pulse sequence
+        s1 = PulseSequence('Relaxation') # dá um nome da medida
+        s1.startup_delay = 1e-6 # um delay para ligar antecipadamente a fonte de excitação. liga a fonte antecipadamente por esse valor antes do primeiro pulso de excitação
+
+        s1.clear()
+
+        s1.add(p1)
+        s1.add(mp,'m') # Adiciona o pulso ao sequenciador e o conecta a um canal, no caso o canal "m"
+        # Esse canal vai ser especificado a um dos canais do awg. Isso ainda não aconteceu
+
+        self.preparePulseSequence(s1)
+
+    def measureRelaxation(self, delays):
+        self.inst_awg.start()
+        self.inst_RFsourceExcitation.start_rf()
+        self.inst_RFsourceMeasurement.start_rf()
+
+
+        # preparar o array de dados capturados
+        Is = np.ndarray(len(delays))
+        Qs = np.ndarray(len(delays))
+
+        Is[:] = 10**(self.backgroundPlotValue/20)
+        Qs[:] = 10**(self.backgroundPlotValue/20)
+
+        for idx,delay in enumerate(delays):
+            clear_output(wait=True)
+
+
+            self.sequence.channels['q']['delays'][0] = delay # change the delay between the pulses
+            
+
+            self.ms.updateChannelData(self.channelData,self.sequence,'Q')
+            
+            self.ms.loadChannelDataToAwg(self.inst_awg,self.channelData,'Q')
+            sleep(0.05)
+
+            self.ms.setInstrumentsMarker(self.inst_awg,self.channelData)
+            sleep(0.05)
+
+            
+
+            I,Q = self.capture()
+
+            Is[idx] = I
+            Qs[idx] = Q 
+            
+            mags = 20*np.log10(np.sqrt(Is**2+Qs**2))
+
+            
+            plt.pause(0.05)
+            plt.plot(delays,mags)
+
+        clear_output(wait=True)
+        plt.pause(0.05)
+        plt.plot(delays,mags)
+
+        self.inst_awg.stop()
+        self.inst_RFsourceExcitation.stop_rf()
+        self.inst_RFsourceMeasurement.stop_rf()
+
+        return Is,Qs,mags
+    
+    def prepareRabiMeasure(self, ExcitionFrequency):
+        self.inst_awg.stop()
+        self.inst_RFsourceMeasurement.stop_rf()
+        self.inst_RFsourceExcitation.stop_rf()
+
+        self.inst_RFsourceExcitation.set_frequency(ExcitionFrequency)
+
+        p1 = SquarePulse(length = self.ExcitationPulseLength) 
+
+        mp = ZeroPulse(length = self.RFMeasurementLength) 
+        
+        # create pulse sequence
+        s1 = PulseSequence('Rabi') # dá um nome da medida
+        s1.startup_delay = 1e-6 # um delay para ligar antecipadamente a fonte de excitação. liga a fonte antecipadamente por esse valor antes do primeiro pulso de excitação
+
+        s1.clear()
+
+        s1.add(p1,'Q',self.RFMeasurementLength) 
+        s1.add(mp,'m') # Adiciona o pulso ao sequenciador e o conecta a um canal, no caso o canal "m"
+        # Esse canal vai ser especificado a um dos canais do awg. Isso ainda não aconteceu
+
+        self.preparePulseSequence(s1)
+
+    def measureRabi(self, durations):
+        self.inst_awg.start()
+        self.inst_RFsourceExcitation.start_rf()
+        self.inst_RFsourceMeasurement.start_rf()
+
+
+        # preparar o array de dados capturados
+        Is = np.ndarray(len(durations))
+        Qs = np.ndarray(len(durations))
+
+        Is[:] = 10**(self.backgroundPlotValue/20)
+        Qs[:] = 10**(self.backgroundPlotValue/20)
+
+        for idx,duration in enumerate(durations):
+            clear_output(wait=True)
+
+
+            self.sequence.channels['q']['pulses'][0].length = duration
+            
+
+            self.ms.updateChannelData(self.channelData,self.sequence,'Q')
+            
+            self.ms.loadChannelDataToAwg(self.inst_awg,self.channelData,'Q')
+            sleep(0.05)
+
+            self.ms.setInstrumentsMarker(self.inst_awg,self.channelData)
+            sleep(0.05)
+
+            
+
+            I,Q = self.capture()
+
+            Is[idx] = I
+            Qs[idx] = Q 
+            
+            mags = 20*np.log10(np.sqrt(Is**2+Qs**2))
+
+            
+            plt.pause(0.05)
+            plt.plot(durations,mags)
+
+        clear_output(wait=True)
+        plt.pause(0.05)
+        plt.plot(durations,mags)
+
+        self.inst_awg.stop()
+        self.inst_RFsourceExcitation.stop_rf()
+        self.inst_RFsourceMeasurement.stop_rf()
+
+        return Is,Qs,mags
