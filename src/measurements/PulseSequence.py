8cc15db0eed913c5534ad5c73e9b087cc83aae8a
Davros <labfdquantum@gmail.com>
2024-05-16 15:37:20 -0300

    added comments

--- src/measurements/PulseSequence.py
+++ src/measurements/PulseSequence.py
@@ -6,11 +6,72 @@ from scipy.signal import convolve
 from measurements.waveforms.Pulse import Pulse
 
 class PulseSequence:
+    '''
+    This class allows one to synchronize the any number of pulses for different channels.
+
+    A channel is just a label. It will be used by the DataChannelManager to build the array of bytes
+    that will be loaded into AWG. 
+
+    '''
     def __init__(self, name):
+        '''
+        Init the pulse sequence.
+        name: name of this sequence of pulses.
+
+        The self.channels is a dict, where channel will one of its keys.
+
+        self.channels[channel.lower()] is also dict that holds three keys: pulses, delays and relative delays
+
+        self.channels[channel.lower()]['pulses'] is a list of pulses instances in the channel
+        self.channels[channel.lower()]['delays'] is a list of delays of each pulse in the channel
+        self.channels[channel.lower()]['relative_delays'] is the starting time of pulse in relation to the end of the pulse sequence.
+        for example, let the sequence in channel 'I' have two pulses, P1 and P2, with delay of 5 microseconds between them, and 2 microsecond after P2.
+        P1 has 1 microsecond of length, P2 has 1 microsecond of length. After that, the sequence repeats. Let the 
+        sequence in channel 'Q' be just a P3, with length 3 microsendos and delay 1.5 microseconds.
+
+        Visually this is the sequence in channel 'I' and 'Q'
+         _______                                     _______
+        |       |                                   |       |           
+        |   P1  |                                   |   P2  |          
+        |       |___________________________________|       |_______
+            1µs                     5µs                1µs      1µs
+        |<----->|<--------------------------------->|<----->|<----->|
+                                                                        sequences repeat
+                                    _____________________               ------->
+                                   |                     |
+                                   |          P3         |
+                                   |                     |__________
+                                              5 µs           1.5 µs
+                                   |<------------------->|<-------->|
+
+        Here is how its organized by PulseSequence
+
+
+        self.channels:
+        {
+            'i': {
+                         'pulses' : [   P1,    P2]
+                         'delays' : [ 5 µs,  1 µs]
+                'relative_delays' : [-8 µs, -2 µs]
+                }
+            'q': {
+                         'pulses' : [     P3]
+                         'delays' : [ 1.5 µs]
+                'relative_delays' : [-6.5 µs]
+                }
+        }
+
+        '''
         self.name = name
-        self.channels = {}
+        self.channels = {} # this dictionary holds the pulses for all channels
 
     def add(self, p, channel, delay = 0.1e-9):
+        '''
+        This adds a pulse to a sequence of pulse of a channel. If the channels does not exist, it will be created.
+        p is a Pulse.
+        delay is an delay put just after the pulse. That is the main way to sync pulses.
+
+        '''
         if isinstance(p, Pulse):
             c = channel.lower ()
 
@@ -28,8 +89,10 @@ class PulseSequence:
         else:
             raise TypeError('p should type Pulse.')
 
-    # TODO
     def create_relative_delays(self,c):
+        '''
+            This calculate the relative delay of a every pulse in a channel.
+        '''
         list_pulse_length = []
         for a_p, a_d in zip(self.channels[c]['pulses'],self.channels[c]['delays']):
             list_pulse_length.append(a_p.length + a_d)
