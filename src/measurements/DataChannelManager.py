31dadc86b47eabd6bb2292446c7a742a521cde3d
Davros <labfdquantum@gmail.com>
2024-05-16 16:27:42 -0300

    added comments

--- src/measurements/DataChannelManager.py
+++ src/measurements/DataChannelManager.py
@@ -13,10 +13,57 @@ class DataChannelManager:
     An instance of this class does two things. It receives a PulseSequence and create a dict called ChannelData,
     which is a structure with the data of th pulses in the format accepted by the AWG.
 
-    To do that is uses and dict called awgChannel which connects the channels defined in the 
+    To do that it uses and dict called awgChannel which connects the channels defined in the 
     PulseSequence to a AWG channels.
 
-    Let
+    Let say that are defined 3 channels 'I', 'Q' and 'm'.
+
+    That could be defined as 
+
+    self.awgChannels
+    {
+        'i' : {
+                            'channel': 1
+                        'markerValue': 2
+              }
+        'q' : {
+                            'channel': 2
+                        'markerValue': 2
+              }
+        'm' : {
+                            'channel': 1
+                        'markerValue': 1
+              }
+    }
+
+    The key channel is related to the actual number of the AWG channel; there are two options: 1 and 2.
+    The key markerValue indicate what marker is related to that channel. The AWG have two markers: 1 and 2.
+
+    Obs: 
+    The AWG have physically 4 channels. They are channel 1, 2, 3 and 4. They have colors.
+    Channel 1 is yellow, 2 is green, 3 is blue and 4 is red. 
+
+    However, not all of them are used.
+    That depends on the mode of operation and the license. Here at the LFDQ, we have license only for
+    two channels, and the other could be used as markers.
+
+    The markers output TTL signals as the sampling rate of the AWG. It is used to sync and start the RF sources.
+    Marker 1 is output at channel 3.
+    Marker 2 is output at channel 4.
+
+    markerValue value should be 0, 1, 2 and 3.
+        0 => no marker, 
+        1 => only marker 1, 
+        2 => only marker 2,
+        3 => both marker 1 and marker 2
+
+
+    the use case of a DataChannelManager instance is
+    define awgChannels with labelAwgChannel
+    the send a PulseSequence to prepareChannelData, which return channelData
+    call allocAwgMemory
+    then, call loadChannelDataToAwg
+
 
 
     '''
@@ -26,8 +73,8 @@ class DataChannelManager:
         self.awg = awg
         self.awgChannels = {}
 
-    def labelAwgChannel(self, channel, channelName, freq, markerValue, markers = False):
-        self.awgChannels[channelName.lower()] = {'channel':channel, 'if_freq': freq, 'measurementMarker': markers, 'markerValue': markerValue}
+    def labelAwgChannel(self, channel, channelName, markerValue):
+        self.awgChannels[channelName.lower()] = {'channel':channel, 'markerValue': markerValue}
 
     def setInstrumentsMarker(self, channelData, marker_value = 1, offset = 0):
         '''
@@ -66,7 +113,6 @@ class DataChannelManager:
     def mergePulseData(self, sequence, channelName, awgRate):
         c = channelName.lower()
         channelInfo = self.awgChannels[c]
-        if_freq = channelInfo['if_freq']
         bytes_amplitude = 127
 
         
@@ -118,12 +164,17 @@ class DataChannelManager:
         c = channelName.lower()
         channelInfo = self.awgChannels[c]
         awgChannel = channelInfo['channel']
-        markers    = channelInfo['measurementMarker']
+
+        areThereMarker = False
+        if awgChannel == 1:
+            areThereMarker = True
+        
+
         markerValue = channelInfo['markerValue']
         
         relative_memory_index, wave_data = self.mergePulseData(sequence,channelName.lower(),channelData['awgRate'])
 
-        channelData['channels'][c] = self.createChannelData(awgChannel, markers, markerValue, wave_data, relative_memory_index)
+        channelData['channels'][c] = self.createChannelData(awgChannel, areThereMarker, markerValue, wave_data, relative_memory_index)
 
         last_relative_delay = 0
         for c in sequence.channels.keys():
@@ -157,13 +208,17 @@ class DataChannelManager:
 
         for (channelName, channelInfo) in self.awgChannels.items():
             awgChannel = channelInfo['channel']
-            markers    = channelInfo['measurementMarker']
+            
+            areThereMarker = False
+            if awgChannel == 1:
+                areThereMarker = True
+            
             markerValue = channelInfo['markerValue']
             
             if channelName in sequence.channels.keys():
                 relative_memory_index, wave_data = self.mergePulseData(sequence,channelName,awgRate)
 
-                all_pulses['channels'][channelName] = self.createChannelData(awgChannel, markers, markerValue, wave_data, relative_memory_index)
+                all_pulses['channels'][channelName] = self.createChannelData(awgChannel, areThereMarker, markerValue, wave_data, relative_memory_index)
                     
     
                     #all_pulses[c][initial_index : initial_index + len(wave)] = wave
